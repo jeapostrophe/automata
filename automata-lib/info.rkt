f0e11a36d8319dcb394d4b10914e26b694534013
Vincent St-Amour <stamourv@racket-lang.org>
2015-07-24 16:09:15 -0500

    Add info.rkt file.

--- automata-lib/info.rkt
+++ automata-lib/info.rkt
@@ -1,3 +1,8 @@
 #lang info
 
+(define collection "automata")
+(define version "1.0")
+(define deps '(("base" #:version "6.2.900.4")))
+(define pkg-authors '(jay))
+
 (define test-responsibles '((all jay)))
