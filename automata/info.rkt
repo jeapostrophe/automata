10b4ff3e401a37c1f88da2c8064f80caa03e550c
Vincent St-Amour <stamourv@racket-lang.org>
2015-07-24 16:25:54 -0500

    Add automata meta-package.

---
+++ automata/info.rkt
@@ -0,0 +1,11 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps '("automata-lib"
+               "automata-doc"))
+(define implies '("automata-lib"
+                  "automata-doc"))
+
+(define version "1.0")
+(define pkg-authors '(jay))
