bce08181105d8689a369b38c38c3468ef338bac1
Vincent St-Amour <stamourv@racket-lang.org>
2015-07-24 16:09:33 -0500

    Move automata docs from unstable repo.

---
+++ automata-doc/info.rkt
@@ -0,0 +1,10 @@
+#lang info
+
+(define collection "automata")
+(define version "1.0")
+(define deps '(("base" #:version "6.2.900.4")
+               "automata-lib"))
+(define build-deps '(("scribble-lib")))
+(define pkg-authors '(jay))
+
+(define scribblings '(("scribblings/automata.scrbl" () (library))))
