8ea5a81c0933a9f2b2dc3dfb52690a8610afccc6
Vincent St-Amour <stamourv@racket-lang.org>
2015-08-06 10:59:05 -0500

    Add missing dependencies.

--- automata-doc/info.rkt
+++ automata-doc/info.rkt
@@ -4,7 +4,9 @@
 (define version "1.0")
 (define deps '(("base" #:version "6.2.900.4")
                "automata-lib"))
-(define build-deps '(("scribble-lib")))
+(define build-deps '("scribble-lib"
+                     "racket-doc"
+                     "unstable-lib"))
 (define pkg-authors '(jay))
 
 (define scribblings '(("scribblings/automata.scrbl" () (library))))
--- automata-lib/info.rkt
+++ automata-lib/info.rkt
@@ -2,7 +2,8 @@
 
 (define collection "automata")
 (define version "1.0")
-(define deps '(("base" #:version "6.2.900.4")))
+(define deps '(("base" #:version "6.2.900.4")
+               "unstable-lib"))
 (define pkg-authors '(jay))
 
 (define test-responsibles '((all jay)))
